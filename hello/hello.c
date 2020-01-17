719431975724efa9e22a3205805842640f029f63
tylerm01 <59982623+tylerm01@users.noreply.github.com>
2020-01-16 18:46:43 -0600

    Create hello.c

---
+++ hello/hello.c
@@ -0,0 +1,27 @@
+/*
+multi-line
+comment
+*/
+//single line comment
+
+
+
+
+#include <stdio.h> //preprocessor directive (includes an external library header file)
+
+//executable code
+//left of main: it returns that type of value
+
+int main(void){   //void(can be left empty) means that no other value is needed for this code
+
+	printf("Tyler Messerschmidt");  //only executable pieces of code require a ";" at the end of the line
+
+	printf("Electrical Engineer");
+
+	return 0; //0(zero) means that the code is done and ready to executed
+
+}
+
+//main: tells the cpu to execute what's inside the block
+
+
